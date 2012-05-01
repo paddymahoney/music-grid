a70e7d6074c3091a0d9dda2f35baf0da0b89a567
Patrick Mahoney <paddy.mahoney@gmail.com>
2012-05-01 13:06:04 -0400

    Remove click-up folder under sizeable.
    It wasn't supposed to be there, probably refactoring legacy.

--- src/gui/ext/sizeable/click-up/click-release-interface.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require racket/gui/base)
-(require racket/class)
-(require (prefix-in mouse: "../mouse-event/mouse-event-interface.rkt"))
-
-(define click-up<%>
-  (interface (mouse:mouse-event<%>) on-click-up))
-
-(provide (all-defined-out))
--- src/gui/ext/sizeable/click-up/left-click-release-interface.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require racket/gui/base)
-(require racket/class)
-(require (prefix-in rls: "click-release-interface.rkt"))
-
-(define left-click-up<%>
-  (interface (rls:click-up<%>) on-left-click-up))
-
-(provide (all-defined-out))
